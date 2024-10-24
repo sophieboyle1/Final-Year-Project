97cf70ca27f885427d9a433eb1b9efe218cb233b
sophieboyle1 <sophieboylemiami@gmail.com>
2024-10-24 11:51:31 +0100

    Add Flask API with app.py to serve data

---
+++ src/app.py
@@ -0,0 +1,17 @@
+from flask import Flask, jsonify
+import pandas as pd
+
+app = Flask(__name__)
+
+@app.route('/api/data', methods=['GET'])
+def get_data():
+    # Load cleaned data
+    data = pd.read_csv('data/hse_scraped_data.csv')
+    
+    # Convert it to JSON format
+    result = data.to_dict(orient='records')
+    
+    return jsonify(result)
+
+if __name__ == '__main__':
+    app.run(debug=True)
