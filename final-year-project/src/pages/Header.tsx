bf91e37c57b212d9dfd93268b8b82b59dcfc86d3
sophieboyle1 <sophieboylemiami@gmail.com>
2024-10-24 14:47:30 +0100

    styled nav and added logo

--- final-year-project/src/pages/Header.tsx
+++ final-year-project/src/pages/Header.tsx
@@ -1,20 +1,22 @@
 import React from 'react';
 import { IonHeader, IonToolbar, IonTitle, IonButtons, IonButton } from '@ionic/react';
 import './Header.css';
+import logo from '../assets/logo.png';
 
 const Header: React.FC = () => {
   return (
     <IonHeader>
       <IonToolbar className="header-toolbar">
         <IonTitle className="header-title">
-          <img src="/assets/logo.png" alt="Logo" className="logo" /> {/* Replace with your logo */}
+        <img src={logo} alt="Logo" className="logo" />
         </IonTitle>
-        <IonButtons slot="end">
-          <IonButton className="nav-button" href="/home">Home</IonButton>
-          <IonButton className="nav-button" href="/data">A&E Data</IonButton>
-          <IonButton className="nav-button" href="/reports">Reports</IonButton>
-          <IonButton className="nav-button" href="/predictions">Predictions</IonButton>
-        </IonButtons>
+        
+        <IonButtons slot="end" className="nav-container">
+    <IonButton className="nav-button" href="/data">A&E Data</IonButton>
+    <IonButton className="nav-button" href="/reports">Reports</IonButton>
+    <IonButton className="nav-button" href="/predictions">Predictions</IonButton>
+</IonButtons>
+
       </IonToolbar>
     </IonHeader>
   );
