64ae041c421f0d40c5740d371b7ed7f3d411f535
sophieboyle1 <sophieboylemiami@gmail.com>
2024-10-27 16:58:15 +0000

    styled text onlanding page, added index.html for google fonts

--- final-year-project/src/pages/Home.tsx
+++ final-year-project/src/pages/Home.tsx
@@ -14,7 +14,8 @@ const HomePage: React.FC = () => {
             <img src={image} alt="A&E Services" className="side-image" />
           </div>
           <div className="text-container">
-            <h1 className="main-title">Welcome to the A&E Waiting Times Dashboard</h1>
+            <h1 className="main-title">Welcome to the A&E</h1>
+            <h1 className="main-title-2">Waiting Times Dashboard</h1>
             <p className="subheading">Stay updated with real-time data and AI-powered predictions.</p>
           </div>
         </div>
