8d39f92f70fe399161b4e9106bd2d8485a3078e2
sophieboyle1 <sophieboylemiami@gmail.com>
2024-10-27 16:39:15 +0000

    added homepage image, text and styled

--- final-year-project/src/pages/Home.tsx
+++ final-year-project/src/pages/Home.tsx
@@ -2,15 +2,22 @@ import React from 'react';
 import { IonContent, IonPage } from '@ionic/react';
 import Header from './Header';
 import './Home.css';
+import image from '../assets/hospital.jpg';
 
 const HomePage: React.FC = () => {
   return (
     <IonPage>
       <Header />
-
       <IonContent className="ion-padding">
-        <h1>Welcome to the A&E Waiting Times Dashboard</h1>
-        <p>Stay updated with real-time data and AI-powered predictions.</p>
+        <div className="homepage-container">
+          <div className="image-container">
+            <img src={image} alt="A&E Services" className="side-image" />
+          </div>
+          <div className="text-container">
+            <h1 className="main-title">Welcome to the A&E Waiting Times Dashboard</h1>
+            <p className="subheading">Stay updated with real-time data and AI-powered predictions.</p>
+          </div>
+        </div>
       </IonContent>
     </IonPage>
   );
