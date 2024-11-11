78fbe9749c290889dbb1c59781466eb87deccbfc
sophieboyle1 <sophieboylemiami@gmail.com>
2024-11-11 17:09:00 +0000

    added stying to reports padata and updated tables to show data thats valid

--- final-year-project/src/pages/Reports.tsx
+++ final-year-project/src/pages/Reports.tsx
@@ -1,8 +1,7 @@
 import React from 'react';
-import { IonContent, IonHeader, IonPage, IonTitle } from '@ionic/react';
+import { IonContent, IonPage } from '@ionic/react';
 import './Reports.css';
 import Header from './Header';
-//import { fetchReportData } from '../dataService';
 
 interface ReportsProps {
   reportData: any[];
@@ -15,28 +14,32 @@ const Reports: React.FC<ReportsProps> = ({ reportData }) => {
       <IonContent className="ion-padding">
         <h1>Daily A&E Reports</h1>
         {reportData.length > 0 ? (
-          <table>
-            <thead>
-              <tr>
-                <th>Date</th>
-                <th>Patients on Trolleys</th>
-                <th>Waiting {'>'} 24 Hours</th>
-                <th>Delayed Transfers</th>
-                <th>Surge Capacity</th>
-              </tr>
-            </thead>
-            <tbody>
-              {reportData.map((item, index) => (
-                <tr key={index}>
-                  <td>{item["Daily Trolley count"] || "N/A"}</td>
-                  <td>{item["Daily Trolley count.1"] || "N/A"}</td>
-                  <td>{item["No of Total Waiting >24hrs"] || "N/A"}</td>
-                  <td>{item["Delayed Transfers of Care (As of Midnight)"] || "N/A"}</td>
-                  <td>{item["Surge Capacity in Use (Full report @14:00)"] || "N/A"}</td>
+          <div className="table-container">
+            <table>
+              <thead>
+                <tr>
+                  <th>Date</th>
+                  <th>Patients on Trolleys</th>
+                  <th>Waiting &gt; 24 Hours</th>
+                  <th>Delayed Transfers</th>
+                  <th>Surge Capacity</th>
                 </tr>
-              ))}
-            </tbody>
-          </table>
+              </thead>
+              <tbody>
+                {reportData.map((item, index) => (
+                  <tr key={index}>
+                    <td>{item["Daily Trolley count"] || "-"}</td>
+                    <td>{item["Daily Trolley count.1"] || "-"}</td>
+                    <td>{item["No of Total Waiting >24hrs"] || "-"}</td>
+                    <td style={{ color: item["Delayed Transfers of Care (As of Midnight)"] > 50 ? 'red' : 'black' }}>
+                      {item["Delayed Transfers of Care (As of Midnight)"] || "-"}
+                    </td>
+                    <td>{item["Surge Capacity in Use (Full report @14:00)"] || "-"}</td>
+                  </tr>
+                ))}
+              </tbody>
+            </table>
+          </div>
         ) : (
           <p>Loading data...</p>
         )}
